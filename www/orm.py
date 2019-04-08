16cb3fe9ca4b92de8ff2f52f9440db279c1398f6
Lam <shadow31night@gmail.com>
2019-04-08 18:53:16 +0800

    repair some problems of orm.py

--- www/orm.py
+++ www/orm.py
@@ -110,23 +110,23 @@ class ModelMetaclass(type):
                  mappings[k] = v
                  if v.primary_key:
                      if primaryKey:
-                         raise RuntimeError('Duplicate primary key for field: %s' % k)
-                    primaryKey = k
-                else:
-                    fields.append(k)
+                         raise Exception('Duplicate primary key for field: %s' % k)
+                     primaryKey = k
+                 else:
+                     fields.append(k)
 
         if not primaryKey:
-            raise RuntimeError('Primary key not found.')
+            raise Exception('Primary key not found.')
         for k in mappings.keys():
             attrs.pop(k)
-        escaped_fields = list(map(lambda f: ''`%s`' % f, fields))
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
         attrs['__mappings__'] = mappings
         attrs['__table__'] = tableName
         attrs['__primary_key__'] = primaryKey
         attrs['__fields__'] = fields
-        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ','.join(escaped_fields), tableName)
-        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ','.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1)
-        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ','.join(map(lambda f:'`%s`=?' % (mapping.get(f).name or f), fields)), primaryKey)
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
         attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
         return type.__new__(cls, name, bases, attrs)
 
@@ -182,7 +182,7 @@ class Model(dict, metaclass=ModelMetaclass):
             else:
                 raise ValueError('Invalid limit value:%s' % str(limit))
         rs = await select(''.join(sql), args)
-        return [cls(**r) for r n rs]
+        return [cls(**r) for r in rs]
 
     @classmethod
     async def findNumber(cls, selectField, where=None, args=None):
